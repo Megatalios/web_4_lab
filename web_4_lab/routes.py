dad6ec1e84eff97fc51787c554022b7a9f40279c
Arsen Sabirzyanov <arpakit@gmail.com>
2024-04-26 11:35:31 +0500

    res

--- web_4_lab/routes.py
+++ web_4_lab/routes.py
@@ -5,44 +5,44 @@ from web_4_lab.service import get_all_clients, get_client, get_client_by_name, r
 from web_4_lab.utils import get_app
 
 
-@get_app().get("/all_clients")
+@get_app().route("/all_clients", methods=['GET'])
 def route_all_clients():
     return safely_transfer_to_json_str({"clients": get_all_clients()})
 
 
-@get_app().get("/")
-@get_app().get("/index")
+@get_app().route("/")
+@get_app().route("/index", methods=['GET'])
 def route_index():
     return render_template("index.html")
 
 
-@get_app().getget("/about")
+@get_app().route("/about", methods=['GET'])
 def route_about():
     return render_template("about_us.html")
 
 
-@get_app().get('/contacts')
+@get_app().route('/contacts', methods=['GET'])
 def route_contacts():
     return render_template("contacts.html")
 
 
-@get_app().get('/success')
+@get_app().route('/success', methods=['GET'])
 def route_success():
     return render_template("success.html")
 
 
-@get_app().get('/feedback')
+@get_app().route('/feedback', methods=['GET'])
 def route_feedback():
     return render_template("feedback.html")
 
 
-@get_app().get('/api/contactrequest/<int:id>', methods=['GET'])
+@get_app().route('/api/contactrequest/<int:id>', methods=['GET'])
 # Получаем запись по id
 def route_get_contact_req_by_id(id: int):
     return safely_transfer_to_json_str(get_client(id))
 
 
-@get_app().get('/api/contactrequest/author/<string:name>', methods=['GET'])
+@get_app().route('/api/contactrequest/author/<string:name>', methods=['GET'])
 # Получаем запись по имени пользователя
 def route_get_client_by_name(name: str):
     name = name.strip()
@@ -52,7 +52,7 @@ def route_get_client_by_name(name: str):
         return make_response(safely_transfer_to_json_str(get_client_by_name(name=name)), 200)
 
 
-@get_app().post('/api/contactrequest')
+@get_app().route('/api/contactrequest', methods=['POST'])
 # Обработка запроса на создание новой записи в БД
 def route_create_client():
     if (
@@ -73,13 +73,13 @@ def route_create_client():
     return make_response(safely_transfer_to_json_str(client_data), 200)
 
 
-@get_app().delete("/api/contactrequest/<int:id>")
+@get_app().route("/api/contactrequest/<int:id>", methods=["DELETE"])
 def route_delete_contact_req_by_id(id: int):
     remove_client_by_id(id)
     return make_response(safely_transfer_to_json_str({'message': "Client was removed"}), 400)
 
 
-@get_app().get("/notfound")
+@get_app().route("/notfound", methods=["GET"])
 def route_not_found():
     return render_template('404.html', title='404', err={'error': 'Not found', 'code': 404})
 
